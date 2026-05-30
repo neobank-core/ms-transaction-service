ec7f0a7e276a6c374b23f6e6ddbb657dd0911b1b
Abbashatred <gitabbas26@gmail.com>
2026-05-30 23:12:36 +0400

    first commit

---
+++ src/main/java/org/neobank/transactionservice/MsTransactionServiceApplication.java
@@ -0,0 +1,13 @@
+package org.neobank.transactionservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsTransactionServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsTransactionServiceApplication.class, args);
+    }
+
+}
